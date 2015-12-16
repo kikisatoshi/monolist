d2ef69b6c2811d5c955c46320d3a6440c5ff9fc7
kikisatoshi <keyofcastle@gmail.com>
2015-12-16 11:52:26 +0000

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
 
-  has_many :following_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following_users, through: :following_relationships, source: :followed
   has_many :followed_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followed_users, through: :followed_relationships, source: :follower
@@ -15,6 +15,11 @@ class User < ActiveRecord::Base
   has_many :ownerships, foreign_key: "user_id", dependent: :destroy
   has_many :items, through: :ownerships
 
+  # class_name"Want":Wantインスタンス(ownershipsテーブルからtypeがWantであるもの)を取得
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -29,22 +34,27 @@ class User < ActiveRecord::Base
     following_users.include?(other_user)
   end
 
-  ## TODO 実装
   def have(item)
+    haves.find_or_create_by(item_id: item.id)
   end
 
   def unhave(item)
+    haves.find_by(item_id: item.id).destroy
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    wants.find_or_create_by(item_id: item.id)
   end
 
   def unwant(item)
+    wants.find_by(item_id: item.id).destroy
   end
 
   def want?(item)
+    want_items.include?(item)
   end
 end
