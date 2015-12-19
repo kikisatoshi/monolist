bc26e7ec04a2f2543fc58dfcb9e56a0c44b829c4
kikisatoshi <keyofcastle@gmail.com>
2015-12-19 15:30:06 +0000

    ranking fourth commit

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,12 +1,12 @@
 class RankingController < ApplicationController
 
   def have
-    haves = Have.limit(10).group(:item_id)
+    haves = Have.limit(10).group('item_id')
     @items = Item.where(id: haves.order('user_id desc').pluck(:item_id))
   end
 
   def want
-    wants = Want.limit(10).group(:item_id)
+    wants = Want.limit(10).group('item_id')
     @items = Item.where(id: wants.order('user_id desc').pluck(:item_id))
   end
 
